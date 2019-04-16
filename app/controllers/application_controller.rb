4f39c0306f2c0449f61f5b749af679e2ed609689
中村　貫太 <sevenstars122@gmail.com>
2019-04-16 14:44:25 +0900

    Procfile編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
+  def after_sign_in_path_for(resource)
+    '/users/show'
+  end
+
+  private
+  def sign_in_required
+    redirect_to new_user_session_url unless user_signed_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 root 'pages#home'
-get  '/about',   to: 'pages#about'
-
+  get  '/about',   to: 'pages#about'
+  get '/users/:id' => 'users#show'
 end
