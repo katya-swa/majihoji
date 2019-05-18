8930a5613f1a337c14240de352e2c53d0b8aac6e
中村　貫太 <sevenstars122@gmail.com>
2019-05-18 14:29:09 +0900

    no message

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   has_many :comments, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { maximum: 200 }
   validate  :picture_size
   mount_uploader :picture, PictureUploader
 
