7d1bee7f36c57043f9a2c3a9d70c90721fc5adf6
matsushin <matsushin.dev@gmail.com>
2019-05-18 22:42:32 +0900

    モデルのリファクタ

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     if user_signed_in?
       @item  = current_user.items.build
-      @feed_items = current_user.feed.page(params[:page]).per(6)
+      @feed_items = current_user.items.page(params[:page]).per(6)
     end
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
-  validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 200 }
   validate  :picture_size
   mount_uploader :picture, PictureUploader
@@ -10,8 +9,6 @@ class Item < ApplicationRecord
   private
 
   def picture_size
-    if picture.size > 5.megabytes
-      errors.add(:picture, "5MB以下にしてください")
-    end
+    errors.add(:picture, "5MB以下にしてください") if picture.size > 5.megabytes
   end
 end
