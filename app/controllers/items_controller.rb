ab2eaadd66be3ae48fdf4fab5e3ae51d0a8005b8
中村　貫太 <sevenstars122@gmail.com>
2019-05-20 15:11:20 +0900

    no message

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,7 +18,7 @@ class ItemsController < ApplicationController
 
   def update
     @item.update!(item_params)
-    redirect_to item_path(item), notice: "投稿を更新しました。"
+    redirect_to item_path(@item), notice: "投稿を更新しました。"
   end
 
   def create
