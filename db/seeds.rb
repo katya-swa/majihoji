ed100a3d41b4ea48e98111eadc9b685784a0922a
中村　貫太 <sevenstars122@gmail.com>
2019-05-27 11:25:06 +0900

    seeds微調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 User.create!(name:  "Katya",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+             email: "test@example.com",
+             password:              "test",
+             password_confirmation: "test",
              admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "test-#{n+1}@example.com"
   password = "password"
   User.create!(name:  name,
                email: email,
