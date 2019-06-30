5855d8cebe3dd97dd1cb93110fe4e44af4f83820
中村　貫太 <sevenstars122@gmail.com>
2019-06-30 11:35:00 +0900

    modelスペックの追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,16 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'paperclip/matchers'
+require 'devise'
+require File.expand_path("spec/support/controller_macros.rb")
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -33,7 +43,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = "#{::Rails.root}/spec/fixtures/test.jpg"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -60,4 +70,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+  config.include Paperclip::Shoulda::Matchers
+  config.include Devise::TestHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
 end
